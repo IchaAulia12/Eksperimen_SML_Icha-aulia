fd0462cf4d0a0554d0eef8c26413c6753607e8f0
IchaAulia12 <ichaaulia720@gmail.com>
2025-05-29 03:35:08 +0700

    preprocessing, automate, notebook

--- preprocessing/automate_Icha-aulia.py
+++ preprocessing/automate_Icha-aulia.py
@@ -1,5 +1,6 @@
-# automate_Nama-siswa.py
+
 import pandas as pd
+import sys
 
 def preprocess_student_data(filepath):
     df = pd.read_csv(filepath)
@@ -9,5 +10,13 @@ def preprocess_student_data(filepath):
     df['parental level of education'] = df['parental level of education'].astype('category').cat.codes
     df['lunch'] = df['lunch'].map({'standard': 1, 'free/reduced': 0})
     df['test preparation course'] = df['test preparation course'].map({'completed': 1, 'none': 0})
-
+    df['passed'] = ((df['math score'] >= 50) &
+                (df['reading score'] >= 50) &
+                (df['writing score'] >= 50)).astype(int)
     return df
+if __name__ == "__main__":
+    input_path = sys.argv[1]
+    output_path = sys.argv[2]
+
+    df = preprocess_student_data(input_path)
+    df.to_csv(output_path, index=False)
